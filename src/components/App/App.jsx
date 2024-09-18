2690f7fc8959db211221256eb7062d3880330374
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-09-18 19:54:21 +0200

    key add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,12 +6,30 @@ import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import ImageModal from "../ImageModal/ImageModal";
 import ImageCard from "../ImageCard/ImageCard";
+import { fetchImagesWithInput } from "../../images - api";
 
 export default function App() {
+	const [images, setImages] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
+
+	const handleSearch = async (input) => {
+		try {
+			setImages([]);
+			setError(false);
+			setLoading(true);
+			const data = await fetchImagesWithInput(input);
+			setImages(data);
+		} catch (error) {
+			setError(true);
+		} finally {
+			setLoading(false);
+		}
+	};
 	return (
 		<div className={css.appContainer}>
+			<SearchBar onSubmit={handleSearch} />
 			<LoadMoreBtn />
-			<SearchBar />
 			<ImageGallery />
 			<Loader />
 			<ErrorMessage />
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,29 @@
 import css from "./SearchBar.module.css";
-export default function SearchBar() {
-	return <p className={css.searchBar}>SearchBar</p>;
+import toast from "react-hot-toast";
+
+export default function SearchBar({ onSubmit }) {
+	const handleSubmit = (evt) => {
+		evt.preventDefault();
+		const form = evt.target;
+		const input = form.elements.input.value;
+
+		if (form.elements.input.value.trim() === "") {
+			toast.error("Please enter search term!");
+			return;
+		}
+		onSubmit(input);
+		form.reset();
+	};
+	return (
+		<header className={css.searchBar}>
+			<form onSubmit={handleSubmit}>
+				<input
+					name="input"
+					type="text"
+					placeholder="Search images and photos"
+				/>
+				<button type="submit">Search</button>
+			</form>
+		</header>
+	);
 }
